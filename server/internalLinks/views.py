4334a26b8225255e77a328b4ae2ec1b844284fee
Tommy Jonathan Sinaga <135734891+TommyJonathanSinaga@users.noreply.github.com>
2023-06-07 22:24:33 +0700

    Create views.py

---
+++ server/internalLinks/views.py
@@ -0,0 +1,20 @@
+from internalLinks.serializers import InternalLinksSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, permissions, viewsets
+
+from .models import InternalLinks
+from .serializers import InternalLinksSerializer
+from .pagination import PageNumberWithPageSizePagination
+
+
+class InternalLinksViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    pagination_class = PageNumberWithPageSizePagination
+    queryset = InternalLinks.objects.all().order_by('-begin_date')
+    serializer_class = InternalLinksSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['id']
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['begin_date']
